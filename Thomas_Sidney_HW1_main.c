fd4e7218ff85c28fefa96bbd13c187add70f4144
Sidney <sid.14boys@yahoo.fr>
2023-08-27 16:37:46 -0700

    debug statement removed + description

--- Thomas_Sidney_HW1_main.c
+++ Thomas_Sidney_HW1_main.c
@@ -7,7 +7,19 @@
  *
  * File: Thomas_Sidney_HW1_main.c
  *
- * Description:
+ * Description: This program demonstrates how to access and print command-line arguments in C.
+ * It takes advantage of the main function's parameters, argumentCount and argumentValues,
+ * to display information about the command-line inputs provided when the program is executed.
+ *
+ * argumentCount: The variable stores the count of command-line arguments passed to the program.
+ * argumentValues: An array of strings containing the values of command-line arguments.
+ * i: A counter variable used for looping through the command-line arguments.
+ *
+ * The program starts by printing the total number of command-line arguments received.
+ * It then uses a loop to iterate through each argument and displays its index and value.
+ * This loop enables the program to process and display multiple arguments systematically.
+ *
+ * Finally, the program concludes by indicating its successful execution and returning 0.
  *
  **************************************************************/
 
@@ -15,27 +27,18 @@
 
 int main(int argumentCount, char *argumentValues[])
 {
-    int i;
-
-    // printf("Start of program\n"); // print a statement indicating the start of the program
-
-    // printf("Number of arguments - argumentCount: %d\n", argumentCount);
-
-    // printf("Program name - argumentValues: %s\n", argumentValues[0]);
+    int i; // counter variable for loops
 
     // print the total number of arguments passed to the program
     printf("There were %d arguments on the command line.\n", argumentCount);
 
     // print the command-line arguments received by the program
-    // printf("ARGV: {");
     for (i = 0; i < argumentCount; i++)
     {
         // print the index of the argument and the argument itself
-        // printf("\"%s\", ", argumentValues[i]);
         printf("Argument 0%d:   ", i);
         printf("%s\n", argumentValues[i]);
     }
-    // printf("\b\b}\n"); // remove the trailing comma and space and close the array
 
     return 0; // indicate successful execution
 }
